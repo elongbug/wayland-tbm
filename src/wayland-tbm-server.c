d677cf410e396940fc39a70618ee27c7a53702d0
SooChan Lim <sc1.lim@samsung.com>
2016-09-21 11:35:59 +0900

    fix the dereference
    Change-Id: I1895b06685f4472ef5e91d3f56c7501cb079e60f

--- src/wayland-tbm-server.c
+++ src/wayland-tbm-server.c
@@ -1131,6 +1131,7 @@ wayland_tbm_server_increase_buffer_sync_timeline(struct wayland_tbm_server *tbm_
 	if (wl_resource_instance_of(wl_buffer, &wl_buffer_interface,
 				    &_wayland_tbm_buffer_impementation)) {
 		tbm_buffer = wl_resource_get_user_data(wl_buffer);
+		WL_TBM_RETURN_IF_FAIL(tbm_buffer != NULL);
 
 		if (tbm_buffer->sync_timeline != -1)
 			tbm_sync_timeline_inc(tbm_buffer->sync_timeline, count);
